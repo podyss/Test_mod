73eb79da783e9ef167073816e1df6db318eac6a8
podyss <18515043822@qq.com>
2023-08-27 22:55:11 +0800

    Initial commit

---
+++ src/main/java/name/test_mod/TestModClient.java
@@ -0,0 +1,10 @@
+package name.test_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
