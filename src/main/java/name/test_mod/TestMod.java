a6d0a89b0133bbbc54bc821793e5c180bbfcf96e
podyss <18515043822@qq.com>
2023-08-27 22:58:23 +0800

    change log text

--- src/main/java/name/test_mod/TestMod.java
+++ src/main/java/name/test_mod/TestMod.java
@@ -17,6 +17,6 @@ public class TestMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Hello Minecraft!");
 	}
 }
